196fa9b22287dcdbe0acd44a35718efb33178e29
Your Name <youremail@example.com>
2026-07-09 01:03:32 +0300

    Второй коммит в проекте

--- app/main.py
+++ app/main.py
@@ -9,9 +9,9 @@ users = [
 ]
 
 @app.get("/users")
-async def get_users():
+def get_users():
     return users
 
 @app.get("/users_count")
-async def get_users_count():
+def get_users_count():
     return {"count": len(users)}
